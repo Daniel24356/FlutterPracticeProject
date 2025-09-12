90c8937f5d777dc13c006e4d8b9ffc81c8a5ecd8
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-12 14:09:13 +0100

    VetDashboard routing, VetAppointment page

---
+++ lib/VetHealthRecords.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class VetHealthRecords extends StatelessWidget {
+  const VetHealthRecords({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
